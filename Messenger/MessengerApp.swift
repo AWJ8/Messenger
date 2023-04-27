d62aa9cbec50df5cb166ff426a638a6a8be57c8f
Aleksander Jasinski <ayyjey8@gmail.com>
2023-04-27 16:34:38 +0100

    no message

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Aleksander Jasinski on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '16.2'
+
+target 'Messenger' do
+  use_frameworks!
+
+  pod 'Socket.IO-Client-Swift', '~> 16.0'
+  pod 'CryptoSwift', '~> 1.4'
+
+end
